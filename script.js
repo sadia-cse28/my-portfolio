85aee99979c4a8599930238dae91f1290fa31b57
sadia-cse28 <sadiaislam79218@gmail.com>
2026-08-18 10:52:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Portfolio JavaScript
+
+console.log("Sadia's Portfolio is running!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Portfolio loaded successfully.");
+});
